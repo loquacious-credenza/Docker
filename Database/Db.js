85aad8a50e1e738334b3600d47dbe7e341a65dfc
Max O'Connell <moconnell7171@gmail.com>
2016-02-11 16:37:44 -0800

    Changed db

--- Database/Db.js
+++ Database/Db.js
@@ -4,6 +4,4 @@ var Mongo = require('../ApiKeys/mongolab.js');
 console.log('Mongo: ', Mongo.MONGO_URL);
 mongoose.connect(Mongo.MONGO_URL);
 
-var db = mongoose.connection;
-
-module.exports = db;
+module.exports = mongoose;
